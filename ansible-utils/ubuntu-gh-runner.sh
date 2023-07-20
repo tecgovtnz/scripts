a2b6738a617d9a1694084a3cff8e204a6ad85c10
Duy Dao <duy.dao@duydao.local>
2023-07-20 13:20:07 +0700

    github runner installation scripts

--- ansible-utils/ubuntu-gh-runner.sh
+++ ansible-utils/ubuntu-gh-runner.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Install Github runner agent
-mkdir actions-runner && cd actions-runner
-# Download the latest runner package
-curl -o actions-runner-linux-x64-2.306.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.306.0/actions-runner-linux-x64-2.306.0.tar.gz
-# Optional: Validate the hash
-echo "b0a090336f0d0a439dac7505475a1fb822f61bbb36420c7b3b3fe6b1bdc4dbaa  actions-runner-linux-x64-2.306.0.tar.gz" | shasum -a 256 -c
-# Extract the installer
-tar xzf ./actions-runner-linux-x64-2.306.0.tar.gz
-
+GITHUB_APP_PRIVATE_KEY=$(echo $GITHUB_APP_PRIVATE_KEY_ENCODED | base64 --decode) 
 # Generate the github runner registration token 
 ACCESS_TOKEN=$(python3 github_app_token.py -o $GITHUB_ORG_NAME -a $GITHUB_APP_ID -p "$GITHUB_APP_PRIVATE_KEY")
 echo TOKEN=$ACCESS_TOKEN
@@ -20,6 +12,15 @@ response=$(curl -X POST \
 
 # Extract the token from the response
 TOKEN=$(echo "$response" | jq -r '.token')
+
+# Install Github runner agent
+mkdir actions-runner && cd actions-runner
+# Download the latest runner package
+curl -o actions-runner-linux-x64-2.306.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.306.0/actions-runner-linux-x64-2.306.0.tar.gz
+# Optional: Validate the hash
+echo "b0a090336f0d0a439dac7505475a1fb822f61bbb36420c7b3b3fe6b1bdc4dbaa  actions-runner-linux-x64-2.306.0.tar.gz" | shasum -a 256 -c
+# Extract the installer
+tar xzf ./actions-runner-linux-x64-2.306.0.tar.gz
 
 # Create the runner and start the configuration experience
 ./config.sh --url https://github.com/tecgovtnz --token $TOKEN --runasservice --name $(hostname) --work _work --runner-group Default --label Linux
