483205b773e084d29fab2a042cb79f4f81142489
Duy Dao <duy.dao@duydao.local>
2023-07-25 06:38:30 +0700

    fix openssl module issue

--- ansible-utils/ubuntu-install-ansible.sh
+++ ansible-utils/ubuntu-install-ansible.sh
@@ -25,3 +25,6 @@ sudo chown ZadockAllen /home/ZadockAllen --recursive
 
 # # Install ansible and azure modules into virtual environment
 # pip3 install -r https://sautilscript.blob.core.windows.net/githubrunner/requirements.txt
+
+wget https://files.pythonhosted.org/packages/f0/e2/f8b4f1c67933a4907e52228241f4bd52169f3196b70af04403b29c63238a/pyOpenSSL-23.2.0-py3-none-any.whl
+python3 -m easy_install pyOpenSSL-23.2.0-py3-none-any.whl
