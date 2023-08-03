b379cb0670a6827ec5e29bb691c1ebb3f43a4904
Duy Dao <duy.dao@duydao.local>
2023-08-03 09:39:59 +0700

    add ansible-galaxyfirewall module

--- ansible-utils/ubuntu-install-ansible.sh
+++ ansible-utils/ubuntu-install-ansible.sh
@@ -20,8 +20,10 @@ sudo apt install python3-pip --assume-yes
 sudo python3 -m venv venv
 sudo chown ZadockAllen /home/ZadockAllen --recursive
 
-# # Install ansible and azure modules into virtual environment
-# pip3 install -r https://sautilscript.blob.core.windows.net/githubrunner/requirements.txt
+# Install ansible and azure modules into virtual environment
+pip3 install -r requirements.txt
+
+ansible-galaxy collection install community.general
 
 wget https://files.pythonhosted.org/packages/f0/e2/f8b4f1c67933a4907e52228241f4bd52169f3196b70af04403b29c63238a/pyOpenSSL-23.2.0-py3-none-any.whl
 python3 -m easy_install pyOpenSSL-23.2.0-py3-none-any.whl
