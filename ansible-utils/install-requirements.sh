3302d0ba21f5a2673a46b30db759eae135785fa4
Reuben Thompson <Reuben.Thompson@tec.govt.nz>
2024-03-08 09:40:24 +1300

    comment out pip install

--- ansible-utils/install-requirements.sh
+++ ansible-utils/install-requirements.sh
@@ -6,7 +6,7 @@ sudo apt install -y python-argparse
 
 
 # install requirements for runner registration
-pip3 install -r requirements.txt
+# pip3 install -r requirements.txt
 
 # install ansible collections
 ansible-galaxy collection install -p '/opt/pipx/venvs/ansible-core/lib/python3.1*/site-packages/ansible_collections' -r collection-requirements.yml --force
