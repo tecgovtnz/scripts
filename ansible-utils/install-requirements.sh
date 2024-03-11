b52c96bcd70fd268d81859b758c4bab9c3ca435a
Reuben Thompson <Reuben.Thompson@tec.govt.nz>
2024-03-12 11:15:19 +1300

    test

--- ansible-utils/install-requirements.sh
+++ ansible-utils/install-requirements.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-#sudo apt-get update
-#sudo apt install -y python3-github
-#sudo apt install -y python-argparse
+sudo apt-get update
+sudo apt install -y python3-github
+sudo apt install -y python-argparse
 
 
 # install requirements for runner registration
 # pip3 install -r requirements.txt
+
+
+python --version
 
 
 installpath=$(echo "/opt/pipx/venvs/ansible-core/lib/python3.1"*"/site-packages/ansible_collections")
