926c83c01f0fb324c36197a16120dcba1195b4bd
Reuben Thompson <Reuben.Thompson@tec.govt.nz>
2024-03-12 10:03:14 +1300

    wildcard test

--- ansible-utils/install-requirements.sh
+++ ansible-utils/install-requirements.sh
@@ -8,5 +8,11 @@ sudo apt install -y python-argparse
 # install requirements for runner registration
 # pip3 install -r requirements.txt
 
+
+installpath=$(echo "/opt/pipx/venvs/ansible-core/lib/python3.1"*"/site-packages/ansible_collections")
+patharray=($installpath)
+
+
 # install ansible collections
-ansible-galaxy collection install -p '/opt/pipx/venvs/ansible-core/lib/python3.1*/site-packages/ansible_collections' -r collection-requirements.yml --force
+ansible-galaxy collection install -p ${patharray[1]} -r collection-requirements.yml --force
+ansible-galaxy collection install
