6551e8b5096a394dc1453c3b28eded3ef9542319
Reuben Thompson <Reuben.Thompson@tec.govt.nz>
2024-03-12 10:20:41 +1300

    test

--- ansible-utils/install-requirements.sh
+++ ansible-utils/install-requirements.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt install -y python3-github
-sudo apt install -y python-argparse
+#sudo apt-get update
+#sudo apt install -y python3-github
+#sudo apt install -y python-argparse
 
 
 # install requirements for runner registration
 # pip3 install -r requirements.txt
 
 
-installpath=$(echo "/opt/pipx/venvs/ansible-core/lib/python3.1"*"/site-packages/ansible_collections")
-patharray=($installpath)
+#installpath=$(echo "/opt/pipx/venvs/ansible-core/lib/python3.1"*"/site-packages/ansible_collections")
+#patharray=($installpath)
 
 
 # install ansible collections
-ansible-galaxy collection install -p ${patharray[1]} -r collection-requirements.yml --force
-ansible-galaxy collection install
+#ansible-galaxy collection install -p ${patharray[1]} -r collection-requirements.yml --force
+#ansible-galaxy collection install
