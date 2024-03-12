85351eda75cce71c2d84585b6bce639c71e3f006
Reuben Thompson <Reuben.Thompson@tec.govt.nz>
2024-03-12 14:35:24 +1300

    test

--- ansible-utils/install-requirements.sh
+++ ansible-utils/install-requirements.sh
@@ -8,10 +8,12 @@
 # install requirements for runner registration
 # pip3 install -r requirements.txt
 
+USR1=ZadockAllen
+PATH=/snap/bin:/home/$USR1/.local/bin:/opt/pipx_bin:/home/$USR1/.cargo/bin:/home/$USR1/.config/composer/vendor/bin:/usr/local/.ghcup/bin:/home/$USR1/.dotnet/tools:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/$USR1/.dotnet/tools
 
+echo $PATH
 python --version
 env
-echo $PATH
 ansible --version
 
 
