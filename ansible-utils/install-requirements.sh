20d8fa3b548d2408c89df661e04de1854a266419
Reuben Thompson <Reuben.Thompson@tec.govt.nz>
2024-03-12 14:56:57 +1300

    test

--- ansible-utils/install-requirements.sh
+++ ansible-utils/install-requirements.sh
@@ -11,17 +11,7 @@
 USR1=ZadockAllen
 PATH=/snap/bin:/home/$USR1/.local/bin:/opt/pipx_bin:/home/$USR1/.cargo/bin:/home/$USR1/.config/composer/vendor/bin:/usr/local/.ghcup/bin:/home/$USR1/.dotnet/tools:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/$USR1/.dotnet/tools
 
-echo $PATH
-python --version
-env
-ansible --version
-
-
-installpath=$(echo "/opt/pipx/venvs/ansible-core/lib/python3.1"*"/site-packages/ansible_collections")
-#patharray=($installpath)
-
+#installpath=$(echo "/opt/pipx/venvs/ansible-core/lib/python3.1"*"/site-packages/ansible_collections")
 
 # install ansible collections
-#ansible-galaxy collection install -p ${patharray[1]} -r collection-requirements.yml --force
-ansible-galaxy collection install -p $installpath -r collection-requirements.yml --force
-ansible-galaxy collection install
+ansible-galaxy collection install -p $(echo "/opt/pipx/venvs/ansible-core/lib/python3.1"*"/site-packages/ansible_collections") -r collection-requirements.yml --force
